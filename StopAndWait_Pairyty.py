5e87eafda566e26a252d2601a878caf8acfc8d1b
Krzysztof Kapała <krzysztof@macbook-pro-krzysztof.home>
2018-04-07 14:34:38 +0200

    pycharm

--- StopAndWait_Pairyty.py
+++ StopAndWait_Pairyty.py
@@ -1,16 +1,15 @@
 from operator import xor
 
 class StopAndWait_Pairyty(object):
-	"""docstring for StopAndWait"""
-	def __init__(self, signal):
+    """docstring for StopAndWait"""
 
-		self.signal = bin(signal)
+    def __init__(self, signal):
+        self.signal = bin(signal)
 
-	def parity(self):
-		pairytyBit = int(self.signal[2])
-		for x in self.signal[3:]:
-			pairytyBit=xor(pairytyBit,int(x))
+    def pairity(self):
+        pairytyBit = int(self.signal[2])
+        for x in self.signal[3:]:
+            pairytyBit = xor(pairytyBit, int(x))
 
-		self.signal=self.signal+str(pairytyBit)
-		return(pairytyBit)
-
+        self.signal = self.signal + str(pairytyBit)
+        return (self.signal)
