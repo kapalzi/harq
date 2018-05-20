66cec2351d85d60dfee0897ddf39629b97dcd306
TheMortys <mortys1000@gmail.com>
2018-05-20 11:11:31 +0200

    CRC changed, may work

--- CRC.py
+++ CRC.py
@@ -3,19 +3,16 @@ from operator import xor
 class CRC:
 
     def CRC(frame):
-        P = 0x04c11db7 #wielomian dla crc-32
-        crc32 = 0xffffffff  #wartosc poczatkowa crc32 (2^32)-1
+        crc = 0
+        crc = 0xffffffff & ~crc
 
-        frame_reversed = frame[::-1] #odwrocenie ramki
-
-        for i in range(0, 24): #rozszerzenie odwroconej ramki
-            frame_reversed.append(0)
-
-        for j in range(0,8):
-            if frame_reversed[j] != 0:
-                frame_reversed[j] =  xor (frame_reversed, P[j])
-
-        frame = frame_reversed[::-1] #ponowne odwrocenie ramki
+        frame = str(frame)
+        for char in frame:
+            byte = ord(char)
+            crc = crc ^ byte
+            for j in range (8):
+                crc = (crc >> 1) ^ (0x04c11db7 & -(crc & 1))
+        return 0xffffffff & ~crc
 
 
 
