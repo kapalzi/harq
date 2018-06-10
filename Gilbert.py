11f05a4185b1add4993e53c23e95f3c5da31daef
krzysztof <235060@student.pwr.edu.pl>
2018-06-10 12:07:09 +0200

    colon

--- Gilbert.py
+++ Gilbert.py
@@ -3,16 +3,22 @@ import random
 class Gilbert(object):
     @staticmethod
     def Gilbert(frame):
-        pdz = random.uniform(0, 1)
-        pzd = random.uniform(0, 1)
-
-        pD = pdz/(pdz+pzd)
-        pZ = pzd/(pzd+pdz)
-
-        if pD>pZ: d = True
-        else: d = False
-
-        return d
-
-
-#a
+        BER1 = 5 #Procentowe prawdopodobienstwo pozostania w stanie dobrym (Wyjscie ze stanu 1-BER1)
+        BER2 = 2 #Procentowe prawdopodobienstwo pozostania w stanie zlym (Wyjscie ze stanu 1-BER2)
+        isValid = True #Status stanu
+        XOR = 1
+        for i in range(0, 8):
+            if isValid:
+                if random.randint(0,99) < BER1:
+                    XOR = XOR * 2
+                    isValid = True
+                else:
+                    isValid = False
+            elif not isValid:
+                if random.randInt(0,99) < BER2:
+                    frame = frame ^ _XOR
+                    _XOR = _XOR * 2
+                    isValid = False
+                else:
+                    isValid = True
+        return frame
